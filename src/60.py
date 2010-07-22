cc72e64202e35c74918b2e39ea455621e6d98da2
duckspeaker <gallo.j@gmail.com>
2010-07-22 19:36:05 -0400

    Added about.

--- src/60.py
+++ src/60.py
@@ -1,7 +1,7 @@
 import gmpy
-import itertools
 
-LIMIT = 1000
+LIMIT = 10000
+n = 5
 
 def concat(x, y):
     return int(str(x)+str(y))
@@ -12,7 +12,7 @@ def concatenable(x, y):
     return gmpy.is_prime(check_one) and gmpy.is_prime(check_two)
 
 def main():
-    global LIMIT
+    global LIMIT, n
     m = {}
     primes = [x for x in range(3, LIMIT) if gmpy.is_prime(x)]
     for x in primes:
@@ -29,27 +29,20 @@ def main():
                     m[y] = set([y])
                 m[y].add(x)
 
-    for x in m.iterkeys():
+    for x in sorted(m.iterkeys()):
         count = 0
         solution = set([x])
-        for y in m[x]:
-            if x != y and len(m[x] & m[y]) >= 5:
+        for y in sorted(m[x]):
+            if x != y and len(m[x] & m[y]) > n:
+                print(x, y)
+                print(sorted(m[x]))
+                print(sorted(m[y]))
+                print('\n')
                 solution.add(y)
                 count += 1
-                if count == 5:
+                if count == n:
                     print(solution)
                     return
-    '''
-    print(sorted([x for x in m[3]]))
-    print(sorted([x for x in m[7]]))
-    print(sorted([x for x in m[109]]))
-    print(sorted([x for x in m[673]]))
-    a = m[3]
-    b = m[7]
-    c = m[109]
-    d = m[673]
-    print(sorted(a & b & c & d))
-    '''
 
 if __name__ == "__main__":
     main()
