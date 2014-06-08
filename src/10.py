7cbd0ba10f938a85d4d3fc7550942293a644f054
Joe Gallo <gallo.j@gmail.com>
2014-06-08 00:54:15 -0400

    improved sieve of erathosthenes

--- src/10.py
+++ src/10.py
@@ -7,8 +7,8 @@ import cProfile
 
 from ProjectEulerLibrary import primeNumbers, sumOfPrimes
 
-primes = primeNumbers(10)
-print(primes)
+# primes = primeNumbers(2000000)
+# print(primes)
 
 # cProfile.run('print(primeNumbers(10))')
-# cProfile.run('print(sumOfPrimes(10))') 
+cProfile.run('print(sumOfPrimes(2000000))') 
--- src/ProjectEulerLibrary.py
+++ src/ProjectEulerLibrary.py
@@ -2,22 +2,14 @@
 import math
 
 def primeNumbers(n):
-    numbers = [x for x in range(2, n+1)]
-    marked = [False for x in numbers]
-    x = p = 2
-    while p:
-        while x <= numbers[-1]:
-            if x != p:
-                marked[x-2] = True
-            x += p
-        try:
-            p_index = next(i for i,x in enumerate(marked) if not x and numbers[i] > p)
-            p = numbers[p_index]
-            x = p ** 2
-        except StopIteration:
-            p = None
-    primes = [x for i,x in enumerate(numbers) if not marked[i]]
-    return primes
+    A = [True for x in range(2,n+1)]
+    for i in range(2, math.ceil(math.sqrt(n))+1):
+        if A[i-2]:
+            j = i ** 2
+            while j <= n:
+                A[j-2] = False
+                j += i
+    return [(i+2) for i,x in enumerate(A) if x]
 
 def sumOfPrimes(n):
     primes = primeNumbers(n)
